667ff768193e183d4255744e106098d43039d665
jmarie21 <johnmarieygot21@gmail.com>
2025-03-23 21:40:04 +0800

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/tasks', function () {
+        return Inertia::render('tasks');
+    })->name('tasks');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
